c0eac3ffa5a5c11cf01f1bba0dc89b5f1938ab11
krispad-dev <kristofer.padoan@icloud.com>
2021-03-13 13:25:57 +0100

    click bugs fixed

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ import { questions } from './data.js'; //Importerar objekt med frågor
 const options = Array.from(document.querySelectorAll('.question-container div'));
 const questionHeader = document.querySelector('.question-container span');
 const questionContainer = document.querySelector('.question-container .header');
+const question = document.querySelector('.question-container');
 const playerOneContainer = document.querySelector('.player-1');
 const playerTwoContainer = document.querySelector('.player-2');
 const playerOneScoreTxt = document.querySelector('#score-1');
 const playerTwoScoreTxt = document.querySelector('#score-2');
 const startBtn = document.querySelector('#startBtn');
+
 let indexCount = 0;
 let current = questions[indexCount];
 
@@ -21,31 +23,37 @@ let playerTwoScore = 0;
 let aCount = 1;
 
 
-questionHeader.innerText = 'Välkommen till denna awesome quiz!'; //Välkomnar spelaren
+questionHeader.innerText = 'Välkommen till denna awesome quiz!'; //Välkomnar spelaren  
 
 startBtn.onclick = () => startQuiz(); //startar spelet
 
 
 const playerSelector = () => { // Väljer spelare och öka progressbar med ett steg.
+
+
     if (playerOneContainer.classList.contains('hidden-class')) {
         activePlayer = 1;
+
+
 
         setTimeout(() => {
             playerOneContainer.classList.remove('hidden-class');
             playerTwoContainer.classList.add('hidden-class');
             aCount++
-
+            question.classList.remove('no-pointer-events')
             startQuiz();
-        }, 2000);
+        }, 1000);
 
     } else {
         playerOneContainer.classList.add('hidden-class');
         activePlayer = 2;
 
+
         setTimeout(() => {
+            question.classList.remove('no-pointer-events')
             playerTwoContainer.classList.remove('hidden-class');
             startQuiz();
-        }, 2000);
+        }, 1000);
     }
 }
 
@@ -85,6 +93,7 @@ const startQuiz = () => { // Kollar om spelare har vunnit eller blivit oavgjort
     options.forEach(item => { // Kollar huruvida aktiv spelare svarar rätt på frågan.
 
         item.onclick = () => {
+            question.classList.add('no-pointer-events')
             if (item.className.substr(0, 4) === currentAnswer) {
                 activePlayer === 1 ? playerOneScore++ : playerTwoScore++;
                 activePlayer === 1 ? a.style.backgroundColor += '#2ecc71' : b.style.backgroundColor += '#2ecc71';
